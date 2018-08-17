533eaa07e0a7b5d4fa6c33f5e1a6aab0a5a842f2
Irving Didier <bider88@gmail.com>
2018-08-16 22:19:50 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MSN Mobile Ionic
+
+### This is a messenger app
+
+This is app is builded with Ionic
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPageModule } from '../pages/login/login.module';
+import { ConversationPageModule } from '../pages/conversation/conversation.module';
+import { ProfilePageModule } from '../pages/profile/profile.module';
+import { AboutPageModule } from '../pages/about/about.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    LoginPageModule,
+    ConversationPageModule,
+    ProfilePageModule,
+    AboutPageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
